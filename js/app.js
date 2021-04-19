b41967eae2c509af353d6fba6931c61d88db30a9
Nagabhushan <nagabhushanr802@gmail.com>
2021-04-19 17:57:50 +0530

    first comit

---
+++ js/app.js
@@ -0,0 +1,125 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => {
+      console.log("service Worker registered", reg);
+    })
+    .catch((err) => {
+      console.log("service Worker not registered", err);
+    });
+}
+
+const AddRecipe = document.getElementById("add-btn");
+const recipetitle = document.getElementById("title");
+const recipeingredients = document.getElementById("ingredients");
+const recipes = document.getElementById("recipes");
+
+(function () {
+  let db = null;
+  let objectStore = null;
+  const request = indexedDB.open("ninjadb", 1);
+  request.addEventListener("error", (e) => {
+    console.log("dberror", e);
+  });
+  request.addEventListener("success", (e) => {
+    db = e.target.result;
+  });
+  request.addEventListener("upgradeneeded", (e) => {
+    db = e.target.result;
+    if (!db.objectStoreNames.contains("ninjastore")) {
+      objectStore = db.createObjectStore("ninjastore", {
+        keyPath: "id",
+      });
+    }
+  });
+  const load = () => {
+    window.onload = setTimeout(() => {
+      let tx = db.transaction("ninjastore", "readonly");
+      tx.oncomplete = (e) => {
+        console.log("reload", e);
+      };
+      tx.onerror = (e) => {
+        console.log("reload", e);
+      };
+      let store = tx.objectStore("ninjastore");
+      let getReq = store.getAll();
+      getReq.onsuccess = (e) => {
+        let request = getReq;
+        recipes.innerHTML = request.result.map((i) => {
+          return `
+                    <div class="card-panel recipe white row">
+                    <img src="https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/preview" alt="recipe thumb">
+                    <div class="recipe-details">
+                        <div class="recipe-title">${i.title}</div>
+                        <div class="recipe-ingredients">${i.ingredients}</div>
+                    </div>
+                    <div class="recipe-delete">
+                        <button class='delete-btn' value=${i.id}><i class="material-icons" >delete_outline</i></button>
+                    </div>
+                    </div>
+                    `;
+        });
+        const Deletebtn = document.querySelectorAll('.delete-btn');
+        Deletebtn.forEach((Delete)=>{
+          Delete.addEventListener('click',() => {
+              let tx = db.transaction("ninjastore", "readwrite");
+              const id = Delete.value;
+              tx.oncomplete = (e) => {
+                console.log("deletedata", e);
+              };
+              tx.onerror = (e) => {
+                console.log("deletedata ", e);
+              };
+              let store = tx.objectStore("ninjastore");
+              let req = store.delete(parseInt(id));
+              req.onsuccess = (e) => {
+                console.log("data deleted");
+                load();
+              };
+              req.onerror = (e) => {
+                console.log("delete-data req error", e);
+              };
+            });
+        })
+      };
+    }, 2000);
+  };
+  load();
+
+  
+  AddRecipe.addEventListener("click", () => {
+    let title = recipetitle.value;
+    let ingredients = recipeingredients.value;
+    let data = {
+      id: Math.floor(Math.random() * 100),
+      title,
+      ingredients,
+    };
+    if (data.title && data.ingredients) {
+      let tx = db.transaction("ninjastore", "readwrite");
+      tx.oncomplete = (e) => {
+        console.log(e);
+      };
+      tx.onerror = (e) => {
+        console.log(e);
+      };
+      let store = tx.objectStore("ninjastore");
+      let query = store.add(data);
+
+      query.onerror = (e) => {
+        console.log("error add");
+      };
+
+      query.onsuccess = (e) => {
+        console.log("success add");
+        title.value = "";
+        ingredients.value = "";
+        alert('Added data Successfully');
+        load();
+      };
+    } else {
+      alert("Add some data please");
+    }
+  });
+
+})();
---
+++ sw.js
@@ -0,0 +1,59 @@
+const staticCacheName = 'site-static-v4';//we have change the name every time we make changes in cached files
+const dynamicCache = 'site-dynamic-v2';
+const cached_array = ['/index.html',
+'https://fonts.gstatic.com/s/materialicons/v83/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+'/js/app.js',
+'/js/materialize.min.js',
+'/pages/fallback.html',
+'/js/ui.js',
+'/css/materialize.min.css',
+'/css/style.css',
+'https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/preview',
+'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+//install service worker
+self.addEventListener('install',(evt)=>{
+    evt.waitUntil(caches.open(staticCacheName).then((cache)=>{
+        cache.addAll(cached_array);
+    }));
+    self.skipWaiting()
+});
+
+//cache size limit function
+const limitCachesize = (name,size)=>{
+    caches.open(name).then(cache=>{
+        cache.keys().then(keys=>{
+            if(keys.length > size){
+                cache.delete(keys[0]).then(limitCachesize(name,size));
+            }
+        })
+    })
+};
+
+//activate event
+self.addEventListener('activate',(evt)=>{
+    evt.waitUntil(caches.keys().then((keys)=>{
+        return Promise.all(keys.filter(key => key !== staticCacheName && key !== dynamicCache)
+        .map(key => caches.delete(key)));
+    }));
+});
+
+//fetch event
+self.addEventListener('fetch',(evt)=>{
+    evt.respondWith(
+        caches.match(evt.request).then((cacheRes)=>{
+            return cacheRes || fetch(evt.request).then((fetchRes)=>{
+                return caches.open(dynamicCache).then((cache) =>{
+                    cache.put(evt.request.url,fetchRes.clone());
+                    limitCachesize(dynamicCache,15);
+                    return fetchRes;
+                })
+            })
+        }).catch(()=>{
+            //here in conditional fallback it checks wether file that is not loading is an html page, if its not an html page then it will return the value -1
+            if(evt.request.url.indexOf('.html') > -1){
+                return caches.match('/pages/fallback.html');
+            }
+        }
+));         
+});
